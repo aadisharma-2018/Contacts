60bdd830b11708eaa677e102ee14f7464e129ce0
Aadi Sharma <aadisharma0226@gmail.com>
2023-07-17 09:37:23 -0400

    changed editcontact

--- Contacts.Maui/Views/EditContactPage.xaml.cs
+++ Contacts.Maui/Views/EditContactPage.xaml.cs
@@ -1,5 +1,7 @@
 namespace Contacts.Maui.Views;
 
+[QueryProperty(nameof(ContactId),"Id")]
+
 public partial class EditContactPage : ContentPage
 {
 	public EditContactPage()
@@ -10,5 +12,13 @@ public partial class EditContactPage : ContentPage
     private void btnCancel_Clicked(object sender, EventArgs e)
     {
         Shell.Current.GoToAsync("..");
+    }
+
+    public string ContactId
+    {
+        set
+        {
+
+        }
     }
 }
