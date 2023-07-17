e63e13f760900e72b939161872d193ded7c4c06c
Aadi Sharma <aadisharma0226@gmail.com>
2023-07-17 14:30:31 -0400

    added contactControls

---
+++ Contacts.Maui/Views/Controls/ContactControl.xaml.cs
@@ -0,0 +1,88 @@
+﻿namespace Contacts.Maui.Views.Controls;
+
+public partial class ContactControl : ContentView
+{
+
+    public event EventHandler<string> OnError;
+    public event EventHandler<EventArgs> OnUpdate;
+    public event EventHandler<EventArgs> OnCancel;
+
+
+    public ContactControl()
+	{
+		InitializeComponent();
+	}
+
+	public string Name
+	{
+		get
+		{
+			return entryName.Text;
+		}
+		set
+		{
+			entryName.Text = value;
+		}
+	}
+
+	public string Email
+	{
+		get
+		{
+			return entryEmail.Text;
+		}
+		set
+		{
+			entryEmail.Text = value;
+		}
+	}
+
+    public string Phone
+    {
+        get
+        {
+            return entryPhone.Text;
+        }
+        set
+        {
+            entryPhone.Text = value;
+        }
+    }
+
+    public string Address
+    {
+        get
+        {
+            return entryAddress.Text;
+        }
+        set
+        {
+            entryAddress.Text = value;
+        }
+    }
+
+    void btnUpdate_Clicked(System.Object sender, System.EventArgs e)
+    {
+        if (nameValidator.IsNotValid)
+        {
+            OnError?.Invoke(sender, "Name is required.");
+            return;
+        }
+
+        if (emailValidator.IsNotValid)
+        {
+            foreach (var error in emailValidator.Errors)
+            {
+                OnError?.Invoke(sender, error.ToString());
+            }
+            return;
+        }
+
+        OnUpdate?.Invoke(sender, e);
+    }
+
+    void btnCancel_Clicked(System.Object sender, System.EventArgs e)
+    {
+        OnCancel?.Invoke(sender, e);
+    }
+}
--- Contacts.Maui/Views/EditContactPage.xaml.cs
+++ Contacts.Maui/Views/EditContactPage.xaml.cs
@@ -28,10 +28,10 @@ public partial class EditContactPage : ContentPage
             contact = ContactRepository.GetContactById(int.Parse(value));
             if (contact != null)
             {
-                entryName.Text = contact.Name;
-                entryEmail.Text = contact.Email;
-                entryPhone.Text = contact.Phone;
-                entryAddress.Text = contact.Address;
+                contactCtrl.Name = contact.Name;
+                contactCtrl.Email = contact.Email;
+                contactCtrl.Phone = contact.Phone;
+                contactCtrl.Address = contact.Address;
             }
             
         }
@@ -39,28 +39,21 @@ public partial class EditContactPage : ContentPage
 
     private void btnUpdate_Clicked(object sender, EventArgs e)
     {
-        if (nameValidator.IsNotValid)
-        {
-            DisplayAlert("Error", "Name is required.", "OK");
-            return; 
-        }
+        
 
-        if (emailValidator.IsNotValid)
-        {
-            foreach(var error in emailValidator.Errors)
-            {
-                DisplayAlert("Error", error.ToString(), "OK");
-            }
-            return;
-        }
-
-        contact.Name = entryName.Text;
-        contact.Email = entryEmail.Text;
-        contact.Phone = entryPhone.Text;
-        contact.Address = entryAddress.Text;
+        contact.Name = contactCtrl.Name;
+        contact.Email = contactCtrl.Email;
+        contact.Phone = contactCtrl.Phone;
+        contact.Address = contactCtrl.Address;
 
         ContactRepository.UpdateContact(contact.ContactId, contact);
         Shell.Current.GoToAsync("..");
 
     }
+
+    void contactCtrl_OnError(System.Object sender, System.String e)
+    {
+        DisplayAlert("Error", e, "OK");
+    }
+
 }
