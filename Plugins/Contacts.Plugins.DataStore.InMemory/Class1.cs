75fd666a6292f729d32ffb355730631aa1ea5bef
Aadi Sharma <aadisharma0226@gmail.com>
2023-07-19 14:02:24 -0400

    big fix

---
+++ Plugins/Contacts.Plugins.DataStore.InMemory/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Contacts.UseCases.PluginInterfaces;
+
+namespace Contacts.Plugins.DataStore.InMemory
+{
+    // All the code in this file is included in all platforms.
+    public class ContactInMemoryRepository : IContactRepository
+    {
+        public Task<List<CoreBusiness.Contact>> GetContactsAsync(string filterText)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
